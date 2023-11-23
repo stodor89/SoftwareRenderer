e24c80e479bfd33e0fbbc0da16d18494604f8427
Todor Samardzhiev <stodor89@gmail.com>
2023-11-23 08:48:16 +0200

    hello world

---
+++ SoftwareRenderer.cpp
@@ -0,0 +1,12 @@
+﻿// SoftwareRenderer.cpp : Defines the entry point for the application.
+//
+
+#include "SoftwareRenderer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
